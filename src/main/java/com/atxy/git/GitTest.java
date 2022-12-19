6ece9f927f666f81b404b9bcb1afe70142f36f4a
hxy <hxy@atxuyu.com>
2022-12-19 22:39:31 +0800

    second commit

--- src/main/java/com/atxy/git/GitTest.java
+++ src/main/java/com/atxy/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atxy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
